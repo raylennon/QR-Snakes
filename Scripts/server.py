18d00e661c211c62d633b8411126951fa5a5e063
Ray Lennon <raylennon27@gmail.com>
2024-09-15 20:06:40 -0400

    quick change

--- Scripts/server.py
+++ Scripts/server.py
@@ -24,7 +24,7 @@ global mostrecent
 
 if not debug:
     options = RGBMatrixOptions()
-    options.rows = 64
+    options.rows = 128
     options.cols = 64
     options.chain_length = 2
     options.parallel = 1
